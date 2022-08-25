63e504dedc8ca88892aeac3dfc2a39e6436505b9
Szobonya Csaba <csaba215@gmail.com>
2022-08-25 17:29:31 +0200

    Example Cleanup

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,19 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { pokemonApi } from './services/pokemon'
 import { chatApi } from './services/chat';
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(chatApi.middleware),
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
