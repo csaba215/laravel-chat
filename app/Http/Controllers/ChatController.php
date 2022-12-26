7130e140b4ab25566b2da6cf0c0bcb43e98c3d0e
Szobonya Csaba <csaba215@gmail.com>
2022-12-26 22:25:57 +0100

    Change ChatController to api controller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,38 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChatGroup;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 class ChatController extends Controller
 {
-    
-    function __construct()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-
+        return ChatGroup::all();
     }
 
-    public static function routes()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        Route::get('/getChat/{id}', [ChatController::class, 'getChat']);
+        //
     }
 
-    public function getChat()
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ChatGroup  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChatGroup $chat)
     {
-        return(['messages'=>
-        [
-            ['userid'=>1,
-                'msg' => 'hello',
-            ],
-            ['userid'=>2,
-            'msg' => 'hello2',
-            ],
-            ['userid'=>3,
-            'msg' => 'hello3',
-            ],
-        ]
-        ]
-    );
+        return $chat;
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ChatGroup  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChatGroup $chat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ChatGroup  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChatGroup $chat)
+    {
+        //
+    }
 }
