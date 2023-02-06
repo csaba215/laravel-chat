6d89033e85fb5ccf41a4554b1e8688f61a0ed7bc
Szobonya Csaba <csaba@szobonya.com>
2023-02-06 20:14:21 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         
     ],
     server: {
-        host: "192.168.56.56",
+        host: "192.168.66.66",
         watch: {
           usePolling: true,
         }
