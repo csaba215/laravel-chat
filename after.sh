a4c537916a5d99a5ce31fcb87a953fb527207c25
Szobonya Csaba <csaba@szobonya.com>
2023-02-18 21:42:27 +0100

    Update packages to laravel 10 Remove inertia-laravel Remove beyondcode/laravel-websockets, replace with socketi

--- after.sh
+++ after.sh
@@ -24,3 +24,6 @@
 
 #increase limit for laravel vite
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+#install socketi
+sudo npm install -g @soketi/soketi
