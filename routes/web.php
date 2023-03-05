813c842603cdff88766e9e77bff73bf24db6601c
Szobonya Csaba <csaba@szobonya.com>
2023-03-05 14:29:56 +0100

    Remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
-
-/*
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-*/
